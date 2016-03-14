73ebe937d7d9246cc88fe4fced4d2f41c38bdaf8
Kerwong <nutsmail@163.com>
2016-03-14 19:55:47 +0800

    update UnderstandingTheJVM 3-5

--- UnderstandingTheJVM/src/com/Main.java
+++ UnderstandingTheJVM/src/com/Main.java
@@ -1,8 +1,13 @@
 package com;
 
+import chapter3.MinorGC;
+
 /**
  * Created by Alasad on 2016/3/11.
  */
 public class Main {
 
+    public static void main(String[] args) {
+        MinorGC.testAllocation();
+    }
 }
